2c94f7972c4229dd01ce3f78326c502aacdf6614
lakhara <siddharth.lakhara@gmail.com>
2022-10-31 23:40:35 +0530

    guessing game: take user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Input your guess: ");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading line");
+    
+    println!("Your guess: {guess}");
 }
