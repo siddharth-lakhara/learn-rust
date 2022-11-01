74aa337166bee5815a8b4e0e544617d963c2a909
lakhara <siddharth.lakhara@gmail.com>
2022-11-01 12:49:44 +0530

    guess number: final program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,23 +1,29 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
-    println!("Input your guess: ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Error reading line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Input your guess: ");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("Your guess: {guess}");
     }
-
-    println!("Your guess: {guess}, secret number: {secret_number}");
 }
